1fbc6d5e71368809b82ddcfd3c281f5ffc21e581
Fellipe Castro <contact@fellipecastro.com>
2014-08-27 19:35:02 -0300

    Define CrawlerItem

--- crawler/items.py
+++ crawler/items.py
@@ -11,4 +11,6 @@ import scrapy
 class CrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
