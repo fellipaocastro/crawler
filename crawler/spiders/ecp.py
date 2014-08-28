69cc855f31cfeb58ee75709316a4f3d74b19ad85
Fellipe Castro <contact@fellipecastro.com>
2014-08-28 08:46:06 -0300

    Update repository

--- crawler/spiders/ecp.py
+++ crawler/spiders/ecp.py
@@ -8,12 +8,17 @@ from crawler.items import EcpItem
 class EcpSpider(scrapy.Spider):
     name = "ecp"
     allowed_domains = ["epocacosmeticos.com.br"]
-    start_urls = ["http://www.epocacosmeticos.com.br/perfumes"]
+    start_urls = ["http://www.epocacosmeticos.com.br/marcas"]
 
     def parse(self, response):
-        for sel in response.css('h3 > a[href$="/p"]'):
+        # for sel in response.css('h3 > a[href$="/p"]'):
+        #     item = EcpItem()
+        #     item['name'] = sel.xpath('text()').extract()
+        #     item['title'] = sel.xpath('@title').extract()
+        #     item['url'] = sel.xpath('@href').extract()
+        #     yield item
+        for sel in response.css('#content_marcas > div > ul'):
             item = EcpItem()
-            item['name'] = sel.xpath('text()').extract()
-            item['title'] = sel.xpath('@title').extract()
-            item['url'] = sel.xpath('@href').extract()
+            item['name'] = sel.xpath('a/text()').extract()
+            item['url'] = sel.xpath('a/@href').extract()
             yield item
