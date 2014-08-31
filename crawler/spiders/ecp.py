0b82638e1f4b9a312c63caaad8838fca1107f3ac
Fellipe Castro <contact@fellipecastro.com>
2014-08-31 01:34:27 -0300

    Update repository

--- crawler/spiders/ecp.py
+++ crawler/spiders/ecp.py
@@ -32,6 +32,9 @@ sl=3d564047-8ff1-4aa8-bacd-f11730c3fce6&cc=4&sm=0&PageNumber=%s"
 
             self.total_brands += 1
 
+            self.log("Brand: %s" % self.sanitize(sel.xpath("text()")
+                                                    .extract()[0]))
+
             self.log("self.total_brands: %s" % self.total_brands)
 
             yield scrapy.Request(
